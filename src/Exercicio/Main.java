64be7a9c51bf021fd3436263f44ecf845f359672
CarlosVillarinho <worksvillarinho@gmail.com>
2025-04-10 09:08:54 -0300

    Projeto Final

--- src/Exercicio/Main.java
+++ src/Exercicio/Main.java
@@ -2,9 +2,11 @@ package Exercicio;
 
 public class Main {
     public static void main(String[] args) {
-        //OBJETO
+
+        //INSTANCIA O OBJETO
         Util util = new Util(); //new Util().menuPrincipal(); - MANEIRA DE SE FAZER COM UMA LINHA
 
+        //INSTANCIANDO O 'menuPrincipal'
         util.menuPrincipal();
     }
 }
